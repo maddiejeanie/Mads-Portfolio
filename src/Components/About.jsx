cf7998d460cf1b82b26607ee56980c6dba9d8aa3
mv <madeleine.vercoe@gmail.com>
2024-04-29 00:18:42 +1000

    edits

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex flex-col md:flex-row justify-between items-center p-8">
-        <div className="md:w-1/2 mb-8 md:mb-0">
+    <div className="flex flex-col h-screen justify-between p-8">
+        <div className="md:w-1/2 md:mb-0">
           <div className="relative">
             <div className="h-64 w-64 rounded-full mx-auto bg-violet-100"></div>
             <img
-              className="absolute top-0 left-0 right-0 bottom-0 mx-auto rounded-full w-60 h-60 object-cover drop-shadow-2xl shadow-2xl  shadow-violet-100	  transparent bg-clip-content"
+              className="absolute top-0 left-0 right-0 bottom-0 mx-auto rounded-full w-60 h-60 object-cover drop-shadow-2xl shadow-2xl  shadow-violet-100	  transparent"
               src="/images/vault-m.png"
               alt="yes fallout"
             />
@@ -22,11 +21,11 @@ function About() {
             I'm a React Developer, passionate about building helpful, functional tech that make life easier.
           </p>
           <p className="text-gray-100 font-mono mb-2">email me at madeleinejnovak@gmail.com</p>
-          <p className="italic mb-4 text-center md:text-left">
+          <p className="italic text-center md:text-left">
             I like coffee, gaming, live music, my two cats - Billy & BonBon, memes, learning new things. Learn more:
           </p>
         </div>
-      </div>
+      
       <div className="flex self-center mb-8 text-center gap-10">
         <a href="https://github.com/maddiejeanie" className="text-gray-600 hover:text-purple-600 transition-colors duration-300 mr-4">
           <i className="fab fa-github fa-2x"></i>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import Mode from './Mode';
 
 function Nav() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 flex justify-around py-4 bg-gray-700 h1 text-3xl uppercase text-violet-300">
+    <div className="fixed bg-opacity-90 top-0 left-0 right-0 z-50 flex justify-around py-4 bg-gray-800 h1 text-3xl uppercase text-violet-300 drop-shadow-2xl">
       <a href="#About">About</a>
       <a href="#Projects">Projects</a>
       <a href="#Contact">Contact</a>
