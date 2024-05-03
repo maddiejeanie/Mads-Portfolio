95dfb277797743b727189291cdce1542969bb161
mv <madeleine.vercoe@gmail.com>
2024-05-03 17:06:49 +1000

    scroll and nav size

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Mode from './Mode';
 
 function Nav({ isDarkMode }) {
-  const navClasses = `border-transparent border-x-4 hover:border-violet-300 hover:border-solid p-4 -mb-1
+  const navClasses = `scroll-mt-20 border-transparent border-x-4 hover:border-violet-300 hover:border-solid p-4 -mb-1
   ${isDarkMode ? 'hover:bg-gray-800' : 'hover:bg-gray-200'}`;
   return (
     <div
-      className={`pt-2 fixed top-0 left-0 right-0 z-50 h1 text-3xl uppercase border-b-4 drop-shadow-2xl bg-opacity-90 ${
+      className={`pt-2 fixed top-0 left-0 right-0 z-50 h1 text-lg lg:text-3xl uppercase border-b-4 drop-shadow-2xl bg-opacity-90 ${
         isDarkMode ? 'text-violet-300 bg-gray-800' : 'text-violet-900 bg-gray-200'
       } border-violet-300`}
     >
