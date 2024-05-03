100619ed94fece071b150234df55b7460ed12da4
mv <madeleine.vercoe@gmail.com>
2024-05-03 18:21:20 +1000

    please god

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -2,10 +2,9 @@ import About from './About';
 import Mode from './Mode';
 import Projects from './Projects';
 import Contact from './Contact';
-import GitHub from './GitHub'
 import Nav from './Nav';
 import Tech from './Tech'
 import { ModeProvider, DarkModeContext } from './ModeContext';
 
 
-export { About, Mode, Projects, Contact, GitHub, Nav, Tech, ModeProvider, DarkModeContext };
+export { About, Mode, Projects, Contact, Nav, Tech, ModeProvider, DarkModeContext };
