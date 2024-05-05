475d4079e71f326482f7d041c425ba168cadc2d8
mv <madeleine.vercoe@gmail.com>
2024-05-05 22:53:52 +1000

    ignorecase

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={` ${isDarkMode ? 'bg-stripes bg-gray-800' : 'bg-violet-100'} `}>
       <Nav isDarkMode={isDarkMode} />
-      <div className={`sm:w-3/4 lg:w-1/2 mx-auto flex flex-col ${isDarkMode ? 'text-purple-200' : 'text-purple-800'} p-4 scroll-snap-align-center pt-16 space-y-16`}>
+      <div className={`sm:w-3/4 lg:w-1/2 mx-auto flex flex-col p-4 scroll-snap-align-center pt-16 space-y-16 ${isDarkMode ? 'text-purple-200' : 'text-purple-800'}`}>
         <About isDarkMode={isDarkMode} />
         <Tech isDarkMode={isDarkMode} />
         <Projects isDarkMode={isDarkMode} />
